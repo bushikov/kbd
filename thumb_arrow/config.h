dd0550e7b012260abd864aefcbf80100be5a7a1e
bushikov <bushikov@users.noreply.github.com>
2024-07-21 04:38:46 +0900

    thumb_arrowの追加

---
+++ thumb_arrow/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define ENCODERS_PAD_A { GP7 }
+#define ENCODERS_PAD_B { GP0 }
+
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP2
+#define I2C1_SCL_PIN GP3
+
+#define AZOTEQ_IQS5XX_TPS43
+
+#define AZOTEQ_IQS5XX_SWIPE_X_ENABLE true
